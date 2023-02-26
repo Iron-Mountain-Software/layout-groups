9f1be360d7353bb04d9066db2fe841c081435198
Chris Monteferrante <chrismonteferrante@Chriss-MacBook-Pro-2.local>
2023-02-26 12:41:48 -0500

    responsive fit

--- Runtime/ResponsiveGridLayoutCellSizeFit.cs
+++ Runtime/ResponsiveGridLayoutCellSizeFit.cs
@@ -1,3 +1,5 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -6,10 +8,8 @@ namespace SpellBoundAR.LayoutGroups
     [ExecuteAlways]
     [RequireComponent(typeof(RectTransform))]
     [RequireComponent(typeof(GridLayoutGroup))]
-    public class ResponsiveGridLayoutCellSize : MonoBehaviour
+    public class ResponsiveGridLayoutCellSizeFit : MonoBehaviour
     {
-        [SerializeField] private float aspectRatio = 1;
-    
         [Header("Cache")]
         private RectTransform _rectTransform;
         private GridLayoutGroup _gridLayoutGroup;
@@ -19,35 +19,43 @@ namespace SpellBoundAR.LayoutGroups
             _rectTransform = GetComponent<RectTransform>();
             _gridLayoutGroup = GetComponent<GridLayoutGroup>();
         }
-
+        
         private void LateUpdate()
         {
             switch (_gridLayoutGroup.constraint)
             {
                 case GridLayoutGroup.Constraint.FixedColumnCount:
                 {
-                    _rectTransform.offsetMin = new Vector2(0, _rectTransform.offsetMin.y);
-                    _rectTransform.offsetMax = new Vector2(0, _rectTransform.offsetMax.y);
+                    int rowCount = transform.childCount / _gridLayoutGroup.constraintCount;
+                    if (transform.childCount % _gridLayoutGroup.constraintCount > 0) rowCount++;
                     float workableWidth = _rectTransform.rect.width
                                           - _gridLayoutGroup.padding.left
                                           - _gridLayoutGroup.padding.right
                                           - _gridLayoutGroup.spacing.x * (_gridLayoutGroup.constraintCount - 1);
-                    float elementWidth = workableWidth / _gridLayoutGroup.constraintCount;
-                    float elementHeight = elementWidth / aspectRatio;
-                    _gridLayoutGroup.cellSize = new Vector2(elementWidth, elementHeight);
+                    float workableHeight = _rectTransform.rect.height
+                                           - _gridLayoutGroup.padding.top
+                                           - _gridLayoutGroup.padding.bottom
+                                           - _gridLayoutGroup.spacing.y * (rowCount - 1);
+                    _gridLayoutGroup.cellSize = new Vector2(
+                        workableWidth / _gridLayoutGroup.constraintCount,
+                        workableHeight / rowCount);
                     break;
                 }
                 case GridLayoutGroup.Constraint.FixedRowCount:
                 {
-                    _rectTransform.offsetMin = new Vector2(_rectTransform.offsetMin.x, 0);
-                    _rectTransform.offsetMax = new Vector2(_rectTransform.offsetMax.x, 0);
+                    int columnCount = transform.childCount / _gridLayoutGroup.constraintCount;
+                    if (transform.childCount % _gridLayoutGroup.constraintCount > 0) columnCount++;
+                    float workableWidth = _rectTransform.rect.width
+                                          - _gridLayoutGroup.padding.left
+                                          - _gridLayoutGroup.padding.right
+                                          - _gridLayoutGroup.spacing.x * (columnCount - 1);
                     float workableHeight = _rectTransform.rect.height
                                            - _gridLayoutGroup.padding.top
                                            - _gridLayoutGroup.padding.bottom
                                            - _gridLayoutGroup.spacing.y * (_gridLayoutGroup.constraintCount - 1);
-                    float elementHeight = workableHeight / _gridLayoutGroup.constraintCount;
-                    float elementWidth = elementHeight * aspectRatio;
-                    _gridLayoutGroup.cellSize = new Vector2(elementWidth, elementHeight);
+                    _gridLayoutGroup.cellSize = new Vector2(
+                        workableWidth / columnCount,
+                        workableHeight / _gridLayoutGroup.constraintCount);
                     break;
                 }
             }
